9f108a83b50a4e6767a8f2d11ba93cff142f926c
darudaniel <habladaru@gmail.com>
2023-10-29 00:55:36 -0500

    safari color bug 5

--- src/app/layout.jsx
+++ src/app/layout.jsx
@@ -13,6 +13,7 @@ export const metadata = {
 export default function RootLayout({ children }) {
   return (
     <html lang="en">
+      <meta name="viewport" content="width=device-width, initial-scale=1"></meta>
       <body className={inter.className}>
         <ClientAuthContextProvider>
           {children}
