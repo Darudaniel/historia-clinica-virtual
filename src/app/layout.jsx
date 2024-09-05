6928c3e3381ae636d33647975b45b5cf10747625
darudaniel <danielelrincon@gmail.com>
2024-09-05 16:59:18 -0500

    cookies manager

--- src/app/layout.jsx
+++ src/app/layout.jsx
@@ -3,6 +3,8 @@ import { Inter } from 'next/font/google'
 import ClientAuthContextProvider from '@/context/ClientAuthContextProvider'
 import {Providers} from "./providers";
 import { GoogleAnalytics } from '@next/third-parties/google'
+import Script from 'next/script';
+
 
 
 const inter = Inter({ subsets: ['latin'] })
@@ -15,6 +17,14 @@ export const metadata = {
 export default function RootLayout({ children }) {
   return (
     <html lang="es">
+      <head>
+      <Script  
+        id="cookies-script"     
+        src={`https://cdn-cookieyes.com/client_data/3bc99ea4d0774b89291afaa9/script.js`}
+        strategy="beforeInteractive" 
+      />
+      
+      </head>
       <meta name="viewport" content="width=device-width, initial-scale=1"></meta>
       <body className={inter.className}>
         <ClientAuthContextProvider>
